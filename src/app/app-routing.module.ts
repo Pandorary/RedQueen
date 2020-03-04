b98d5e80e34561304350e5a6378dedbbca4e8988
root <panyeheju@163.com>
2020-03-04 22:02:33 +0800

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { StoreCenterComponent } from './store/store-center/store-center.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'store',
+    component:StoreCenterComponent,
+    loadChildren:'./store/store.module#StoreModule'
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { PrimeNGModule } from './primeng.module';
+import { StoreModule } from './store/store.module';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +17,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    PrimeNGModule,
+    FormsModule,
+    HttpClientModule,
+    StoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
